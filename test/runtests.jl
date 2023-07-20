e25d49bf8c96b10e20ffe872b85603d25ee054df
Maël Clergue <mael.clergue@gmail.com>
2023-07-20 15:50:44 +0200

    formating

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+using Vensim2MTK, Test, ModelingToolkit
+
+filepaths = ["..\\examples\\Dice.xmile", "..\\examples\\Lokta.xmile", "..\\examples\\commitment2.xmile", "..\\examples\\community corona 8.xmile"]
+for filepath in filepaths
+    vensim2MTK(filepath, splitext(basename(filepath))[1] * ".jl", true)
+end
+@testset verboe = true "Vensim2MTK.jl" begin
+    @test isa(Dice.solved, ModelingToolkit.ODESystem)
+    @test isa(Lokta.solved, ModelingToolkit.ODESystem)
+    @test isa(commitment.solved, ModelingToolkit.ODESystem)
+    @test isa(community_corona_8.solved, ModelingToolkit.ODESystem)
+end
+
