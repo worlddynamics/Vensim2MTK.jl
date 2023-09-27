43906d4791f8b5e44142b4db63960cc5dbd90f04
Aurora Rossi <65721467+aurorarossi@users.noreply.github.com>
2023-09-27 17:16:31 +0200

    Fix typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,13 @@
 using Vensim2MTK, Test, ModelingToolkit
 
-filepaths=["..\\examples\\Dice.xmile","..\\examples\\Lokta.xmile","..\\examples\\commitment2.xmile","..\\examples\\community corona 8.xmile"]
+filepaths=["..\\examples\\Dice.xmile","..\\examples\\Lotka.xmile","..\\examples\\commitment2.xmile","..\\examples\\community corona 8.xmile"]
 for filepath in filepaths 
     vensim2MTK(filepath, splitext(basename(filepath))[1] * ".jl",true)
 end
 @testset verbose=true "Test output type" begin
 
     @test isa(Dice.solved, ModelingToolkit.ODESystem)
-    @test isa(Lokta.solved, ModelingToolkit.ODESystem)
+    @test isa(Lotka.solved, ModelingToolkit.ODESystem)
     @test isa(commitment.solved, ModelingToolkit.ODESystem)
     @test isa(community_corona_8.solved, ModelingToolkit.ODESystem)
 end
